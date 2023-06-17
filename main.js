be59a28a6598b9a5dc5088d925af1d6ae60396c3
Fantom <132481244+Fantom-it@users.noreply.github.com>
2023-06-17 21:35:01 +0300

    Update main.js

--- main.js
+++ main.js
@@ -74,6 +74,9 @@ function menu() {
   document
     .getElementById("arrow-2")
     .classList.toggle("show-menu__content__cricle-arrow2");
+  document
+    .getElementById("content")
+    .classList.toggle("show-personal-acc-main-page__content-width");
 }
 function menuMob() {
   document.getElementById("menu-mob").classList.toggle("show-menu-mob");
@@ -82,4 +85,52 @@ function menuMob() {
     .classList.toggle("show-menu-mob-menu");
   document.getElementById("crestik").classList.toggle("show-crestik");
 }
+
+const password = document.querySelector("#password");
+const toggleVisibility = document.querySelector("#toggle-visibility");
+let state = new Map([
+  ["password", "text"],
+  ["text", "password"],
+]);
+toggleVisibility.addEventListener("click", () => {
+  password.setAttribute("type", state.get(password.getAttribute("type")));
+});
+
+const password2 = document.querySelector("#password2");
+const toggleVisibility2 = document.querySelector("#toggle-visibility2");
+let state2 = new Map([
+  ["password", "text"],
+  ["text", "password"],
+]);
+toggleVisibility2.addEventListener("click", () => {
+  password2.setAttribute("type", state2.get(password2.getAttribute("type")));
+});
+
+const password3 = document.querySelector("#password3");
+const toggleVisibility3 = document.querySelector("#toggle-visibility3");
+let state3 = new Map([
+  ["password", "text"],
+  ["text", "password"],
+]);
+toggleVisibility3.addEventListener("click", () => {
+  password3.setAttribute("type", state2.get(password3.getAttribute("type")));
+});
+
+function glaz() {
+  document
+    .getElementById("toggle-visibility")
+    .classList.toggle("show-button-glaz");
+}
+function glaz2() {
+  document
+    .getElementById("toggle-visibility2")
+    .classList.toggle("show-button-glaz");
+}
+function glaz3() {
+  document
+    .getElementById("toggle-visibility3")
+    .classList.toggle("show-button-glaz");
+}
+
+
 
