32b8dd670e46c7c54d8c42d4f4c6cc6057b9e2c2
Fantom <132481244+Fantom-it@users.noreply.github.com>
2023-06-23 18:52:41 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 function menu() {
-  document
-    .getElementById("button-arrow-bbb-ggg")
-    .classList.toggle("show-button-arrow-blue");
-  document.getElementById("img-arrow").classList.toggle("show-img-arrow");
+ 
   document.getElementById("menu").classList.toggle("show-menu");
   document.getElementById("menu").classList.toggle("show-menu");
   document
