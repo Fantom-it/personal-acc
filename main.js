7837443b03b68c10c06630b2cf8ccac613dc469c
Fantom <132481244+Fantom-it@users.noreply.github.com>
2023-06-29 01:21:51 +0300

    Update main.js

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ function menu() {
     .classList.toggle("show-button-arrow-blue");
   document.getElementById("img-arrow").classList.toggle("show-img-arrow"); 
   document.getElementById("img-arrow-2").classList.toggle("show-img-arrow-2");
-/*   document.getElementById("img-arrow-3").classList.toggle("show-img-arrow-2");
-  document.getElementById("arrow-page").classList.toggle("show-img-arrow"); */
+   document.getElementById("img-arrow-3").classList.toggle("show-img-arrow-2");
+ /* document.getElementById("arrow-page").classList.toggle("show-img-arrow"); */
  
   document.getElementById("menu").classList.toggle("show-menu");
   document
